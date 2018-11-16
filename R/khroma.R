f226a317121aeeb1131208ddedc827cbb02ee0c6
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2018-11-16 16:34:14 +0100

    Add package description

---
+++ R/khroma.R
@@ -0,0 +1,32 @@
+#' Colour Schemes for Archaeological Data Visualization
+#'
+#' Provides Paul Tol's colour schemes and several thematic palettes (geologic
+#' timescale, etc.) with scales for \code{ggplot2}.
+#'
+#' \tabular{ll}{
+#'  \strong{Package:} \tab khroma \cr
+#'  \strong{Type:} \tab Package \cr
+#'  \strong{Version:} \tab 1.0.0.9000 \cr
+#'  \strong{Date:} \tab 2018-11-16 \cr
+#'  \strong{License:} \tab GPL-3 \cr
+#'  \strong{DOI:} \tab \href{https://doi.org/10.5281/zenodo.1472077}{10.5281/zenodo.1472077}
+#' }
+#'
+#' @name khroma-package
+#' @aliases khroma-package khroma
+#' @docType package
+#' @author \strong{Full list of authors and contributors} (alphabetic order)
+#'  \tabular{ll}{
+#'   Nicolas Frerebeau \tab \emph{IRAMAT-CRP2A, Université Bordeaux Montaigne, France} \cr
+#'   Brice Lebrun \tab \emph{CEREGE, Aix-Marseille Université, France}
+#'  }
+#'
+#' \strong{Bug reporting}
+#'
+#' - \url{https://github.com/nfrerebeau/khroma/issues}
+#'
+#' \strong{Package maintainer}
+#'
+#' Nicolas Frerebea, IRAMAT-CRP2A, Universite Bordeaux Montaigne, Pessac, France,\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+NULL
