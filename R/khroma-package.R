e66d504cb92ed8f68445186af139754472f2c810
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-07-28 18:12:50 +0200

    Update package description

--- R/khroma-package.R
+++ R/khroma-package.R
@@ -2,8 +2,7 @@
 #' \tabular{ll}{
 #'  \strong{Package:} \tab khroma \cr
 #'  \strong{Type:} \tab Package \cr
-#'  \strong{Version:} \tab 1.1.3 \cr
-#'  \strong{Date:} \tab 2019-06-05 \cr
+#'  \strong{Version:} \tab 1.1.4 \cr
 #'  \strong{License:} \tab GPL-3 \cr
 #'  \strong{DOI:} \tab \href{https://doi.org/10.5281/zenodo.1472077}{10.5281/zenodo.1472077}
 #' }
@@ -29,9 +28,3 @@
 #' @docType package
 #' @keywords internal
 "_PACKAGE"
-
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
-## usethis namespace: start
-## usethis namespace: end
-NULL
