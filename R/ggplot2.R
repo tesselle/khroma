1c3bea219a7bebba1f6c3b3c87cb04d95d15cf9f
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-06-09 16:59:08 +0200

    Fix conflicting argument in scale_discrete()

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -11,8 +11,8 @@ NULL
 #'  used (see [colour()]).
 #' @param reverse A [`logical`] scalar: should the resulting vector of colors
 #'  should be reversed?
-#' @param label A [`logical`] scalar: should the names of the colors should be
-#'  kept in the resulting vector?
+#' @param use_names A [`logical`] scalar: should the names of the colors be
+#'  preserved?
 #' @param lang A [`character`] string specifying the language for the color
 #'  names. It must be one of "`en`" (english, the default) or "`fr`" (french).
 #' @param type A [`character`] string specifying the scale to be
@@ -33,9 +33,10 @@ NULL
 NULL
 
 scale_discrete <- function(aesthetics, scale_name, reverse = FALSE,
-                           labels = FALSE, lang = "en", ...) {
+                           use_names = FALSE, lang = "en", ...) {
   # Get colour scheme
-  palette <- colour(scale_name, reverse = reverse, names = labels, lang = lang)
+  palette <- colour(scale_name, reverse = reverse, names = use_names,
+                    lang = lang)
 
   # Build scale
   scale_arguments <- list(...)
