c8ac4b6d093c390fbdd54a5e12bba48967f88c80
Ulrik Stervbo <ulrik.stervbo@gmail.com>
2023-04-11 20:14:52 +0200

    Add and handle function argument `black_position`

--- R/scale_colour_okabeito.R
+++ R/scale_colour_okabeito.R
@@ -28,10 +28,27 @@ NULL
 #' @rdname scale_okabeito_discrete
 NULL
 
+#' @param black_position A [`character`] string giving the position of the black colour.
+#'
+#' @keyword Internal
+#' @examples
+#' get_okabeito_scale("first")
+#' get_okabeito_scale("last")
+#' @rdname scale_okabeito_discrete
+get_okabeito_scale <- function(black_position = c("first", "last")){
+  black_position <- match.arg(black_position, several.ok = FALSE)
+  if(black_position == "first"){
+    return("okabeito")
+  }else{
+    return("okabeito_black_last")
+  }
+}
+
 #' @export
 #' @rdname scale_okabeito_discrete
-scale_colour_okabeito <- function(..., reverse = FALSE, aesthetics = "colour") {
-  scale_discrete(aesthetics, "okabeito", reverse = reverse, ...)
+scale_colour_okabeito <- function(..., reverse = FALSE, black_position = c("first", "last"), aesthetics = "colour") {
+  oi_palette <- get_okabeito_scale(black_position)
+  scale_discrete(aesthetics, oi_palette, reverse = reverse, ...)
 }
 
 #' @export
@@ -40,15 +57,18 @@ scale_color_okabeito <- scale_colour_okabeito
 
 #' @export
 #' @rdname scale_okabeito_discrete
-scale_fill_okabeito <- function(..., reverse = FALSE, aesthetics = "fill") {
-  scale_discrete(aesthetics, "okabeito", reverse = reverse, ...)
+scale_fill_okabeito <- function(..., reverse = FALSE, black_position = c("first", "last"), aesthetics = "fill") {
+  oi_palette <- get_okabeito_scale(black_position)
+  scale_discrete(aesthetics, oi_palette, reverse = reverse, ...)
 }
 
 #' @export
 #' @rdname scale_okabeito_discrete
 scale_edge_colour_okabeito <- function(..., reverse = FALSE,
+                                       black_position = c("first", "last"),
                                        aesthetics = "edge_colour") {
-  scale_discrete(aesthetics, "okabeito", reverse = reverse, ...)
+  oi_palette <- get_okabeito_scale(black_position)
+  scale_discrete(aesthetics, oi_palette, reverse = reverse, ...)
 }
 
 #' @export
@@ -58,6 +78,8 @@ scale_edge_color_okabeito <- scale_edge_colour_okabeito
 #' @export
 #' @rdname scale_okabeito_discrete
 scale_edge_fill_okabeito <- function(..., reverse = FALSE,
+                                     black_position = c("first", "last"),
                                      aesthetics = "edge_fill") {
-  scale_discrete(aesthetics, "okabeito", reverse = reverse, ...)
+  oi_palette <- get_okabeito_scale(black_position)
+  scale_discrete(aesthetics, oi_palette, reverse = reverse, ...)
 }
