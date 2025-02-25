83f2766cbc6497281a1c019d472215138dfb9653
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-02-25 17:32:57 +0100

    Remove deprecated functions

--- R/khroma-defunct.R
+++ R/khroma-defunct.R
@@ -6,19 +6,3 @@
 #' @rdname khroma-defunct
 #' @keywords internal
 NULL
-
-#' @export
-#' @rdname khroma-defunct
-scale_colour_logical <- function(..., aesthetics = "colour") {
-  .Defunct("scale_color_manual", package = "ggplot2")
-}
-
-#' @export
-#' @rdname khroma-defunct
-scale_color_logical <- scale_colour_logical
-
-#' @export
-#' @rdname khroma-defunct
-scale_fill_logical <- function(..., aesthetics = "fill") {
-  .Defunct("scale_color_manual", package = "ggplot2")
-}
--- R/khroma-deprecated.R
+++ R/khroma-deprecated.R
@@ -18,36 +18,3 @@ palette_size_range <- function(range = c(1, 6), ...) {
     scale_range(sqrt(x), to = range(range, na.rm = TRUE))
   }
 }
-
-#' @export
-#' @rdname khroma-deprecated
-convert <- function(x, mode) {
-  .Deprecated(new = "change()", old = "convert()")
-  change(x, mode)
-}
-
-#' @export
-#' @rdname khroma-deprecated
-ramp <- function(palette, ...) {
-  .Deprecated(new = "palette_color_picker()", old = "ramp()")
-
-  fun <- function(x, midpoint = NULL) {
-    if (!is.null(midpoint) && is.numeric(midpoint)) {
-      z <- scale_midpoint(x, midpoint = midpoint)
-    } else {
-      z <- (x - min(x)) / (max(x) - min(x))
-    }
-
-    col <- color(palette, ...)
-    ramp <- grDevices::colorRamp(col(255))(z)
-
-    ## Set attributes
-    structure(
-      grDevices::rgb(ramp[, 1], ramp[, 2], ramp[, 3], maxColorValue = 255),
-      breaks = x,
-      class = c("color_scheme")
-    )
-  }
-
-  return(fun)
-}
