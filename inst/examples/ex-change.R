0077f6e21b9c663a380e0069ab1b4695ce520446
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-02-25 17:53:46 +0100

    Ignore examples if spacesXYZ is not installed

--- inst/examples/ex-change.R
+++ inst/examples/ex-change.R
@@ -1,23 +1,24 @@
-# Trichromat
-pal <- colour("bright")
-plot_scheme(pal(7))
+if (requireNamespace("spacesXYZ", quietly = TRUE)) {
+  ## Trichromat
+  pal <- colour("bright")
+  plot_scheme(pal(7))
 
-# Deuteranopia
-deu <- change(pal, mode = "deuteranopia")
-plot_scheme(deu(7))
+  ## Deuteranopia
+  deu <- change(pal, mode = "deuteranopia")
+  plot_scheme(deu(7))
 
-# Protanopia
-pro <- change(pal, mode = "protanopia")
-plot_scheme(pro(7))
+  ## Protanopia
+  pro <- change(pal, mode = "protanopia")
+  plot_scheme(pro(7))
 
-# Tritanopia
-tri <- change(pal, mode = "tritanopia")
-plot_scheme(tri(7))
+  ## Tritanopia
+  tri <- change(pal, mode = "tritanopia")
+  plot_scheme(tri(7))
 
-# Achromatopsia
-ach <- change(pal, mode = "achromatopsia")
-plot_scheme(ach(7))
+  ## Achromatopsia
+  ach <- change(pal, mode = "achromatopsia")
+  plot_scheme(ach(7))
 
-## Plot simulated color blindness
-plot_scheme_colorblind(pal(7))
-
+  ## Plot simulated color blindness
+  plot_scheme_colorblind(pal(7))
+}
--- inst/examples/ex-compare.R
+++ inst/examples/ex-compare.R
@@ -1,19 +1,21 @@
-# Trichromat
-pal <- colour("bright")
-compare(pal(5))
+if (requireNamespace("spacesXYZ", quietly = TRUE)) {
+  ## Trichromat
+  pal <- colour("bright")
+  compare(pal(5))
 
-# Deuteranopia
-deu <- change(pal, mode = "deuteranopia")
-compare(deu(5))
+  ## Deuteranopia
+  deu <- change(pal, mode = "deuteranopia")
+  compare(deu(5))
 
-# Protanopia
-pro <- change(pal, mode = "protanopia")
-compare(pro(5))
+  ## Protanopia
+  pro <- change(pal, mode = "protanopia")
+  compare(pro(5))
 
-# Tritanopia
-tri <- change(pal, mode = "tritanopia")
-compare(tri(5))
+  ## Tritanopia
+  tri <- change(pal, mode = "tritanopia")
+  compare(tri(5))
 
-# Achromatopsia
-ach <- change(pal, mode = "achromatopsia")
-compare(ach(5))
+  ## Achromatopsia
+  ach <- change(pal, mode = "achromatopsia")
+  compare(ach(5))
+}
